7fc131c1699028ee53fe1e4547cfc2d10cf11e2b
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-11 01:01:30 +0300

    User created using cookies and redis

--- src/api/player.ts
+++ src/api/player.ts
@@ -1,16 +1,33 @@
 import express from "express"
+import { generateId } from "../engine/id_generator"
+import { redisClient } from "../redis"
+
 const router = express.Router()
 
 router.post('/', (req, res) => {
-    // req.username
-    // res.addCookie
-    res.send("Player created, cookie sent")
+    if (!req.signedCookies["user_id"])
+    {
+        const userName = req.body.username
+        const id = generateId(userName,process.env.URL)
+        
+        redisClient.SET(id,userName)
+        res.cookie("user_name", userName, { signed: true })
+        res.cookie("user_id", id, { signed: true })
+
+        res.send("Player created, cookie sent")
+    }
 })
 
 router.delete('/', (req,res) => {
-    // req.username
-    // req.removeCookie
-    res.send("Player deleted, cookie deleted")
+    const id = req.signedCookies["user_id"]
+    
+    if (id)
+    {
+        redisClient.DEL(id)
+        res.clearCookie("user_name", { signed: true })
+        res.clearCookie("user_id", { signed: true })
+        res.send("Player deleted, cookie deleted")
+    }
 })
 
 export default router
