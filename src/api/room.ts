71ab4834fee4e7ce092ee3d91a0565af3efe458a
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-10 15:01:30 +0300

    Aesthetic changes to code

--- src/api/room.ts
+++ src/api/room.ts
@@ -1,37 +1,37 @@
 import express from "express"
 const router = express.Router()
 
-router.post('/', ( req, res ) => {
-    //req.gametype
-    //req.player
+router.post('/', (req, res) => {
+    // req.gametype
+    // req.player
     res.send( "Room created" );
 } );
 
 router.post('/player', ( req, res ) => {
-    //req.player
-    //req.room
+    // req.player
+    // req.room
     res.send( "Player joined room" );
 } );
 
 router.delete('/player', ( req, res ) => {
-    //req.player
-    //req.room
+    // req.player
+    // req.room
     res.send( "Player left room" );
 } );
 router.put('/leader', ( req, res ) => {
-    //req.player
-    //req.room
-    //req.newleader
+    // req.player
+    // req.room
+    // req.newleader
     res.send( "Leader changed" );
 } );
 
 router.put('/gametype', ( req, res ) => {
-    //req.isPlayerMaster
+    // req.isPlayerMaster
     res.send( "game changed" );
 } );
 
 router.get('/gametype', ( req, res ) => {
-    //req.isPlayerinRoom?
+    // req.isPlayerinRoom?
     res.send( "Game type is:" );
 } );
 
