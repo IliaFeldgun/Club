05bd19eedf11674ce132dc025e0d20f87a0ac4ab
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-11 00:02:14 +0300

    Changed function name to a more generic one

--- src/engine/id_generator.ts
+++ src/engine/id_generator.ts
@@ -1,5 +1,5 @@
 import {v5 as uuidv5} from "uuid"
 
-export function generateUuidv5(name: string, url: string) : string {
+export function generateId(name: string, url: string) : string {
     return uuidv5(name, url)
 }
