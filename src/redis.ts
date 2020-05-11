6a2d5a1dd37d0d474c4eb19062a77dd38c388cb6
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-11 16:23:37 +0300

    Generalised Redis use

--- src/redis.ts
+++
@@ -1,6 +0,0 @@
-import redis from "redis"
-export let redisClient = redis.createClient({
-    port: Number.parseInt(process.env.REDIS_PORT, 10),
-    host: process.env.REDIS_HOST,
-    password: process.env.REDIS_PASSWORD,
-})
