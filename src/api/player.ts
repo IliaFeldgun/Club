0f86ded03624e721847977811346bb69be3669e7
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-11 17:37:14 +0300

    Changed user to player

--- src/api/player.ts
+++ src/api/player.ts
@@ -5,28 +5,28 @@ import store from "../engine/key_value_state_store"
 const router = express.Router()
 
 router.post('/', (req, res) => {
-    if (!req.signedCookies["user_id"]) {
-        const userName = req.body.username
-        const id = generateId(userName,process.env.UUID_PLAYER_NAMESPACE)
+    if (!req.signedCookies["player_id"]) {
+        const playerName = req.body.playerName
+        const id = generateId(playerName,process.env.UUID_PLAYER_NAMESPACE)
         
-        store.set()(id,userName)
-        res.cookie("user_name", userName, { signed: true })
-        res.cookie("user_id", id, { signed: true })
+        store.set()(id,playerName)
+        res.cookie("player_name", playerName, { signed: true })
+        res.cookie("player_id", id, { signed: true })
 
         res.send("Player created, cookie sent")
     }
     else {
-        res.send("You are already " + req.signedCookies["user_name"])
+        res.send("You are already " + req.signedCookies["player_name"])
     }
 })
 
 router.delete('/', (req,res) => {
-    const id = req.signedCookies["user_id"]
+    const id = req.signedCookies["player_id"]
     
     if (id) {
         store.delete()(id)
-        res.clearCookie("user_name", { signed: true })
-        res.clearCookie("user_id", { signed: true })
+        res.clearCookie("player_name", { signed: true })
+        res.clearCookie("player_id", { signed: true })
         res.send("Player deleted, cookie deleted")
     }
     else {
