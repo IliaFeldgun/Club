655c9d271379f59d2bf1883c5a905b22c17ff68e
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-11 17:38:00 +0300

    Added room creation and player addition logic

--- src/api/room.ts
+++ src/api/room.ts
@@ -1,16 +1,39 @@
 import express from "express"
+import { generateId } from "../engine/id_generator"
+import store from "../engine/key_value_state_store"
 const router = express.Router()
 
 router.post('/', (req, res) => {
-    // req.gametype
-    // req.player
-    res.send( "Room " +req.body.roomName+ " created" );
-} );
+    const playerId = req.signedCookies["player_id"]
+    
+    if (playerId) {
+        const roomId = generateId(playerId, process.env.UUID_ROOM_NAMESPACE)
+        store.set()(roomId, JSON.stringify({leader: playerId, players: []}))
+        res.send( "Room " + roomId + " created" );
+    }
+    else {
+        res.status(403)
+        res.send("Room can't be created, you need to set a player")
+    }
+});
 
 router.post('/player', ( req, res ) => {
-    // req.player
-    // req.room
-    res.send( "Player joined room" );
+    const playerId = req.signedCookies["player_id"]
+    if (playerId) {
+        const roomId = req.body.roomId
+        
+        if (roomId) {
+            store.get()(roomId, (err, reply) => {
+                let room = JSON.parse(reply)
+                room.players.push(playerId)
+                store.set()(roomId, JSON.stringify(room))
+            })
+        }
+    }
+    else {
+        res.status(403)
+        res.send("Room can't be joined, you need to set a player")
+    }
 } );
 
 router.delete('/player', ( req, res ) => {
