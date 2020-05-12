da9762fb1680d69578498b253896d3f681eaf609
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-12 20:01:07 +0300

    Remove unnecessary dependencies

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import CardFan from './components/CardFan';
 import CardBoard from './components/CardBoard';
 import ScoreBoard from './components/ScoreBoard';
@@ -22,8 +20,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
