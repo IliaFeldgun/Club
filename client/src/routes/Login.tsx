1f553631eae7ed84abfc6054c9c8d19ed04b3cfa
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-13 18:09:33 +0300

    Designed basic login form

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import './Login.css';
 
 export default class Login extends React.PureComponent {
     render() {
+        const fieldClass = "form-field"
+        const allClass = "centered"
+        const buttonClass = "form-button"
         return (
             <React.Fragment>
-                <h1>Login</h1>
+                <div className={allClass}>
+                    <h3>Login with a name of your choice</h3>
+                    <form id="loginform" action="/api/player">
+                        <input className={fieldClass} id="loginname" type="text" name="loginname" />
+                        <button className={buttonClass} id="loginsend" type="submit">
+                            <span>
+                                Login
+                            </span>
+                        </button>
+                    </form>
+                </div>
             </React.Fragment>
         )
     }
