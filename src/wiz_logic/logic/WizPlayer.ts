cc695ecf89c1e207ae10b8d92557eee203a071c8
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-13 19:56:37 +0300

    Fixed compilation errors, mock anyway

--- src/wiz_logic/logic/WizPlayer.ts
+++ src/wiz_logic/logic/WizPlayer.ts
@@ -1,14 +1,14 @@
 import { WizScore } from "./WizScore"
 
-export class WizPlayer extends Player implements IWizPlayer {
+export class WizPlayer implements IWizPlayer {
+    id: string
+    name: string
     stack: IStack
     score: IWizScore
     currentBet: IWizBet
     currentRoundResult: IWizPlayerRoundResult
 
     constructor(name: string, roomId: string) {
-        super(name, roomId)
-
         this.stack = new Stack([])
         this.score = new WizScore()
         this.currentBet = undefined
