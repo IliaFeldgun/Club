e496352d36ed810beed66fe3c985e7f95d8db6bf
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-13 03:52:32 +0300

    Lint fixes

--- src/engine/room_logic/models/Room.ts
+++ src/engine/room_logic/models/Room.ts
@@ -2,7 +2,7 @@ interface IRoom {
     id: string
     leader: IPlayer["id"]
     players: IPlayer["id"][]
-    //addPlayer: (player: IPlayer) => void
-    //removePlayer: (player: IPlayer) => void
-    //close: () => void
+    // addPlayer: (player: IPlayer) => void
+    // removePlayer: (player: IPlayer) => void
+    // close: () => void
 }
