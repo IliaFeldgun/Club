d370e3d6550fd9c922b0aaa21e1f10b5467dc61e
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-13 19:55:46 +0300

    Adde promisify to redisclient logic

--- src/engine/key_value_state_store.ts
+++ src/engine/key_value_state_store.ts
@@ -1,4 +1,5 @@
 import { redisClient } from "./redis"
+import { promisify } from "util"
 
 export default class KeyValueStateStore {
     static set() {
@@ -11,5 +12,17 @@ export default class KeyValueStateStore {
 
     static get() {
         return redisClient.GET
+    }
+
+    static setAsync() {
+        return promisify(redisClient.SET).bind(redisClient)
+    }
+
+    static deleteAsync() {
+        return promisify(redisClient.DEL).bind(redisClient)
+    }
+
+    static getAsync() {
+        return promisify(redisClient.GET).bind(redisClient)
     }
 }
