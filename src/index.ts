710df024c88ae407f8ce5976a6f3ac5c280d0735
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-13 18:10:50 +0300

    Configured React client route for deployment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv"
 dotenv.config()
 
+import path from 'path'
 import express from "express"
 import cookieParser from "cookie-parser"
 import game from "./api/game"
@@ -14,18 +15,25 @@ const port = process.env.PORT; // default port to listen
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET))
 
+app.use(express.static('./client/build'))
+
 app.use("/api/game", game)
 app.use("/api/player", player)
 app.use("/api/room", room)
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
+/*app.get( "/", ( req, res ) => {
     res.send( "Hello world!" );
 } );
-
+*/
 app.get( "/api", (req,res) => {
     res.send( "This is the API")
 })
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile('index.html', {root: "./client/build"});
+});
 
 // start the Express server
 app.listen( port, () => {
