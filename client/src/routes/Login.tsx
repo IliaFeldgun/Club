134727c3ddb845bfcf200d8c8f237c9ae88d841e
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-13 03:53:03 +0300

    Login component for routing

---
+++ client/src/routes/Login.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Login extends React.PureComponent {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Login</h1>
+            </React.Fragment>
+        )
+    }
+}
