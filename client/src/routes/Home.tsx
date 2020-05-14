e8bbd0b18da60ba5f76f21c852c6ae2a433ee931
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-14 15:25:50 +0300

    Basic room creation on home screen

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,10 +1,38 @@
 import React from "react";
 
 export default class Home extends React.PureComponent {
+    constructor(){
+        super({})
+        this.roomCreate = this.roomCreate.bind(this)
+    }
+    roomCreate(e: React.MouseEvent) {
+        fetch("/api/room", {
+            method: 'POST',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then((res) => {
+            res.json().then((res) =>
+            {
+                
+            })
+        })
+    }
+
     render() {
+        const allClass = "centered"
+        const buttonClass = "form-button"
         return (
             <React.Fragment>
-                <h1>Home</h1>
+                <div className={allClass}>
+                    <h3>Create a room</h3>
+                    <button className={buttonClass} id="roomcreate" type="button" onClick={this.roomCreate}>
+                        <span>
+                            Create a room for me
+                        </span>
+                    </button>
+                </div>
             </React.Fragment>
         )
     }
