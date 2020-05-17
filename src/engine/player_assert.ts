eb723d6d98c30af1054f8a525e64e02641ae4e9e
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-17 15:55:57 +0300

    Moved types to their own file

--- src/engine/player_assert.ts
+++ src/engine/player_assert.ts
@@ -1,9 +1,5 @@
 import express, { Request, Response, NextFunction } from "express"
-declare module 'express' {
-    export interface Request {
-        playerId?: string
-    }
-}
+
 export default function assertPlayer(req : Request, res: Response, next: NextFunction) {
     if (req.signedCookies.player_id) {
         req.playerId = req.signedCookies.player_id
