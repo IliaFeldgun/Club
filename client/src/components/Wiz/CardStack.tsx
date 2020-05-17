bf6fa6b24801859420b6bccc9ce449e92db56b36
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-17 19:25:45 +0300

    Created a stack for the game

--- client/src/components/Wiz/CardStack.tsx
+++ client/src/components/Wiz/CardStack.tsx
@@ -1,3 +1,15 @@
-export class CardStack {
-    
+import React from "react";
+import FlippedCard from "./FlippedCard";
+
+export default class CardStack extends React.PureComponent {
+    render() {
+        const classes = "stack"
+        return (
+        <React.Fragment>
+            <div className={classes}>
+                <FlippedCard/>
+            </div>
+        </React.Fragment>
+        )
+    }
 }
