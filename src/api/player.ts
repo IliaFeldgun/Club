519ac90edc0e87b17ac650c3a07a28904be5eed5
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-17 16:23:32 +0300

    Converted to async-await

--- src/api/player.ts
+++ src/api/player.ts
@@ -5,35 +5,43 @@ import Player from "../engine/room_logic/logic/Player"
 
 const router = express.Router()
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     if (!req.signedCookies.player_id) {
         const playerName = req.body.playerName
         const id = generateId(playerName,process.env.UUID_PLAYER_NAMESPACE)
         // TODO: decouple
         const player : IPlayer = new Player(id, playerName)
 
-        store.setAsync()(player.id, JSON.stringify(player)).then((ok: any) => {
-                res.cookie("player_name", player.name, { signed: true })
-                res.cookie("player_id", player.id, { signed: true })
-                res.send("Player created, cookie sent") }
-            ).catch((err: any) => {
-                res.status(500)
-                res.send("FAIL")
-            })
+        try {
+            const storeResponse = await store.setAsync()(player.id, JSON.stringify(player))
+            res.cookie("player_name", player.name, { signed: true })
+            res.cookie("player_id", player.id, { signed: true })
+            res.send("Player created, cookie sent")
+        }
+        catch(error) {
+            res.status(500)
+            res.send("FAIL")
+        }
     }
     else {
         res.send("You are already " + req.signedCookies.player_name)
     }
 })
 
-router.delete('/', (req,res) => {
-    const id = req.signedCookies.player_id
+router.delete('/', async (req,res) => {
+    const playerId = req.playerId
 
-    if (id) {
-        store.delete()(id)
-        res.clearCookie("player_name", { signed: true })
-        res.clearCookie("player_id", { signed: true })
-        res.send("Player deleted, cookie deleted")
+    if (playerId) {
+        try {
+            const storeResponse = await store.deleteAsync()(playerId)
+            res.clearCookie("player_name", { signed: true })
+            res.clearCookie("player_id", { signed: true })
+            res.send("Player deleted, cookie deleted")
+        }
+        catch (error) {
+            res.status(500)
+            res.send("FAIL")
+        }
     }
     else {
         res.send("You are not any player (No cookies)")
