52e89a860912af86a3b292022753ef2cc20070b2
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-19 02:07:31 +0300

    Typed card

--- client/src/components/Wiz/CardStack.tsx
+++ client/src/components/Wiz/CardStack.tsx
@@ -4,7 +4,9 @@ import Card, { ICardProps } from "./Card";
 
 interface ICardStackProps {
     cards: Array<{suit: ICardProps["suit"], rank: ICardProps["rank"]}>
-    handleCardClick?: (event: React.MouseEvent, suit: string, rank: string) => void
+    handleCardClick?: (event: React.MouseEvent, 
+                       suit: ICardProps["suit"], 
+                       rank: ICardProps["rank"]) => void
 }
 interface ICardStackState {
 
@@ -15,7 +17,7 @@ export default class CardStack extends React.PureComponent<ICardStackProps,ICard
         
         this.handleCardClick = this.handleCardClick.bind(this)
     }
-    handleCardClick(event: React.MouseEvent, suit: string, rank: string) {
+    handleCardClick(event: React.MouseEvent, suit: ICardProps["suit"], rank: ICardProps["rank"]) {
         if (this.props.handleCardClick)
             this.props.handleCardClick(event, suit, rank)
     }
