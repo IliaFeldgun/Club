9c8ae7f776eaa40098e611c615d4254749b20266
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-19 17:49:20 +0300

    Round advancement coded

--- src/wiz_logic/WizMaster.ts
+++ src/wiz_logic/WizMaster.ts
@@ -1,75 +1,79 @@
-import WizGame from "./logic/WizGame";
-import IWizGame from "./models/WizGame";
-import Deck from "../card_logic/logic/Deck";
-import Stack from "../card_logic/logic/Stack";
-import IRoom from "../engine/room_logic/models/Room";
 import IPlayer from "../engine/room_logic/models/Player";
-import { WizScore } from "./logic/WizScore";
-import WizPlayerRoundResult from "./logic/WizPlayerRoundResult";
 import ICard from "../card_logic/models/Card";
 import WizGameRules from "./logic/WizGameRules";
 import IWizRound from "./models/WizRound";
-import WizRound from "./logic/WizRound";
-import WizBet from "./logic/WizBet";
 
 export default class WizMaster {
-    static newGameState(gameId: IWizGame["id"],
-                        roomId: IRoom["id"],
-                        players: IPlayer["id"][]) : IWizGame {
+    static playCard(round: IWizRound, 
+                    cardPlayed: ICard,
+                    playerId: IPlayer["id"]): IWizRound 
+    {        
+        if (WizMaster.canPlayCard(round, cardPlayed, playerId)) {
+            const cardsLeft = round.playerHands[playerId].filter(card =>
+                card.equals(cardPlayed))
 
+            round.playerHands[playerId] = cardsLeft
 
-        const game = new WizGame(gameId, roomId)
-        players.forEach((player) => {
-            game.playerScores[player] = new WizScore()
-        })
-
-        return game
+            WizMaster.advanceRound(round)
+            //if (WizMaster.areAllHandsEmpty(round))
+            return round
+        }
+        else 
+            return null
     }
-    static newGameRound(roundId: IWizRound["id"],
-                        gameId: IWizGame["id"],
-                        players: IPlayer["id"][]) {
-        const roundDeck = new Deck(true)
-        const roundTableStack = new Stack([])
-
-        const round = new WizRound(roundId, gameId, roundDeck, roundTableStack)
-
-        players.forEach((player) => {
-            // round.playerBets[player] =
-            round.playerHands[player] = new Stack([])
-        })
-    }
-
-    static canPlayCard(round: IWizRound,
+    static canPlayCard(round: IWizRound, 
                        cardPlayed: ICard,
-                       playerId: IPlayer["id"]): boolean {
-        const isCardInHand = round.playerHands[playerId].contains(cardPlayed)
+                       playerId: IPlayer["id"]): boolean 
+    {
+        const isCurrentPlayer = playerId === WizMaster.getCurrentPlayer(round)
+        const isCardInHand = -1 !== round.playerHands[playerId].findIndex(card => 
+            cardPlayed.equals(card)) 
 
-        const playerCards = round.playerHands[playerId].cards
+        const playerCards = round.playerHands[playerId]
         const topCard = round.tableStack.top()
         const requiredSuit = WizGameRules.getRequiredSuit(round.tableStack.cards)
         const isMoveValid = WizGameRules.checkPlayValidity(cardPlayed,
             playerCards, topCard, requiredSuit)
 
-        return isCardInHand && isMoveValid
+        return isCurrentPlayer && isCardInHand && isMoveValid
 
-    }
-    static playCard(round: IWizRound,
-                    cardPlayed: ICard,
-                    playerId: IPlayer["id"]): IWizRound {
-
-        if (this.canPlayCard(round, cardPlayed, playerId)) {
-            const cardsLeft = round.playerHands[playerId].cards.filter(card =>
-                card.equals(cardPlayed))
-
-            round.playerHands[playerId].cards = cardsLeft
-
-            this.advanceRound(round)
-        }
-        else {
-            return null
-        }
     }
     static advanceRound(round: IWizRound) {
-        const newRound = {}
+        
+        this.assertWinner(round)
+
+        round.turnNumber++
+    }
+    static assertWinner(round: IWizRound) {
+        if (WizMaster.didAllPlay(round)) {
+            const requiredSuit = 
+                WizGameRules.getRequiredSuit(round.tableStack.cards)
+
+            const winningCard = 
+                WizGameRules.getWinningCard(round.tableStack.cards, requiredSuit)
+
+            const winningPlayer = WizMaster.getPlayerByCard(round, winningCard)
+
+            round.playerResults[winningPlayer].addTake()
+        }
+    }
+    static getPlayerByCard(round: IWizRound, card: ICard): IPlayer["id"] {
+        const cardIndex = round.tableStack.indexOf(card)
+        return round.playerOrder[cardIndex]
+    }
+    static getCurrentPlayer(round: IWizRound) {
+        const currentPlayerNum = round.turnNumber % round.playerOrder.length
+        
+        return round.playerOrder[currentPlayerNum - 1]
+    }
+    static didAllPlay(round: IWizRound): boolean {
+        return round.turnNumber % round.playerOrder.length == 0
+
+    }
+    static areAllHandsEmpty(round: IWizRound): boolean {
+        const playerHands = Object.entries(round.playerHands)
+        const allEmpty = !playerHands.some(([player,cards]) => cards.length)
+
+        return allEmpty
     }
 }
