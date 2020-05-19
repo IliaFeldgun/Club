815154e62624c79419610125f10e946c64df7175
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-19 20:51:17 +0300

    Removed non-promisified functions

--- src/engine/key_value_state_store.ts
+++ src/engine/key_value_state_store.ts
@@ -2,18 +2,6 @@ import { redisClient } from "./redis"
 import { promisify } from "util"
 
 export default class KeyValueStateStore {
-    static set() {
-        return redisClient.SET
-    }
-
-    static delete() {
-        return redisClient.DEL
-    }
-
-    static get() {
-        return redisClient.GET
-    }
-
     static setAsync() {
         return promisify(redisClient.SET).bind(redisClient)
     }
