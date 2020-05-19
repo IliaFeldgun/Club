69bac0391f30219663caced112a2bd9b7d41b3ed
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-19 17:50:08 +0300

    Changed round state members

--- src/wiz_logic/logic/WizRound.ts
+++ src/wiz_logic/logic/WizRound.ts
@@ -5,24 +5,29 @@ import IStack from "../../card_logic/models/Stack";
 import IWizBet from "../models/WizBet";
 import IPlayer from "../../engine/room_logic/models/Player";
 import IWizPlayerRoundResult from "../models/WizPlayerRoundResult";
+import ICard from "../../card_logic/models/Card";
 
 export default class WizRound implements IWizRound {
     id: string
+    roundNumber: number
+    turnNumber: number
     gameId: IWizGame["id"]
     deck: IDeck
     tableStack: IStack
-    currentPlayer: IPlayer["id"];
-    playerHands: { [playerId: string]: IStack }
+    playerOrder: IPlayer["id"][]
+    playerHands: { [playerId: string]: ICard[] }
     playerBets: { [playerId: string]: IWizBet }
     playerResults: { [playerId: string]: IWizPlayerRoundResult}
 
-    constructor(id: string, gameId: IWizGame["id"],
+    constructor(id: string, gameId: IWizGame["id"], roundNumber: number,
                 deck: IDeck, tableStack: IStack) {
         this.id = id
+        this.roundNumber = roundNumber
+        this.turnNumber = 1
         this.gameId = gameId
         this.deck = deck
         this.tableStack = tableStack
-        this.currentPlayer = ""
+        this.playerOrder = []
         this.playerHands = {}
         this.playerBets = {}
 
--- src/wiz_logic/models/WizRound.ts
+++ src/wiz_logic/models/WizRound.ts
@@ -4,14 +4,17 @@ import IDeck from "../../card_logic/models/Deck";
 import IWizGame from "./WizGame";
 import IPlayer from "../../engine/room_logic/models/Player";
 import IWizPlayerRoundResult from "./WizPlayerRoundResult";
+import ICard from "../../card_logic/models/Card";
 
 export default interface IWizRound {
     id: string
+    roundNumber: number
+    turnNumber:number
     gameId: IWizGame["id"]
     deck: IDeck
     tableStack: IStack
-    currentPlayer: IPlayer["id"]
-    playerHands: { [playerId: string]: IStack }
+    playerOrder: IPlayer["id"][]
+    playerHands: { [playerId: string]: ICard[] }
     playerBets: { [playerId: string]: IWizBet }
     playerResults: { [playerId: string]: IWizPlayerRoundResult}
 }
