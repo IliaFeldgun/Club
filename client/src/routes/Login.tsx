b3700d026f59afb69770cfe966422026cdc19fca
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-20 20:53:40 +0300

    Added click handler

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import './Login.css';
+import { useHistory } from 'react-router-dom'
+interface ILoginProps {
 
-export default class Login extends React.PureComponent {
+}
+export default class Login extends React.PureComponent<ILoginProps,{}> {
+    constructor(props: ILoginProps) {
+        super(props)
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick(event: React.MouseEvent) {
+        
+    }
     render() {
         const fieldClass = "form-field"
-        const allClass = "centered"
+        const allClass = "centered-top"
         const buttonClass = "form-button"
         return (
             <React.Fragment>
@@ -12,7 +22,7 @@ export default class Login extends React.PureComponent {
                     <h3>Login with a name of your choice</h3>
                     <form id="loginform" method="POST" action="/api/player">
                         <input className={fieldClass} id="playerName" type="text" name="playerName" />
-                        <button className={buttonClass} id="loginsend" type="submit">
+                        <button className={buttonClass} id="loginsend" type="submit" onClick={this.handleClick}>
                             <span>
                                 Login
                             </span>
