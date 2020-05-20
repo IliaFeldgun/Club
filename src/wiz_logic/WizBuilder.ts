8d6ea8edb8775a9f037e4cc4bd0474f38b8beff0
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-20 20:45:46 +0300

    Linting and naming changes

--- src/wiz_logic/WizBuilder.ts
+++ src/wiz_logic/WizBuilder.ts
@@ -9,10 +9,10 @@ import Stack from "../card_logic/logic/Stack"
 import WizRound from "./logic/WizRound"
 
 export default class WizBuilder {
-    
+
     static newGameState(gameId: IWizGame["id"],
                         roomId: IRoom["id"],
-                        players: IPlayer["id"][]) : IWizGame 
+                        players: IPlayer["id"][]) : IWizGame
     {
         const game = new WizGame(gameId, roomId)
         players.forEach((player) => {
@@ -21,19 +21,19 @@ export default class WizBuilder {
 
         return game
     }
-    static newGameRound(roundId: IWizRound["id"],
+    static newRoundState(roundId: IWizRound["id"],
                         gameId: IWizGame["id"],
                         roundNumber: number,
                         players: IPlayer["id"][],
-                        firstPlayer: IPlayer["id"]): IWizRound 
+                        firstPlayer: IPlayer["id"]): IWizRound
     {
         const roundDeck = new Deck(true)
         const roundTableStack = new Stack([])
 
         const round = new WizRound(roundId, gameId, roundNumber, roundDeck, roundTableStack)
-        
+
         round.playerOrder = WizBuilder.generatePlayerOrder(firstPlayer, players)
-        
+
         players.forEach((player) => {
             round.playerHands[player] = []
         })
@@ -41,8 +41,8 @@ export default class WizBuilder {
         return round
     }
 
-    static generatePlayerOrder(firstPlayer: IPlayer["id"], 
-                               players: IPlayer["id"][]): IPlayer["id"][] 
+    static generatePlayerOrder(firstPlayer: IPlayer["id"],
+                               players: IPlayer["id"][]): IPlayer["id"][]
     {
         const newOrder = [...players]
 
