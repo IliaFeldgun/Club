e37cc8e191a7efc340d76cd993bf561efae53a61
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-22 19:45:13 +0300

    Decoupled route and logic of login

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -1,34 +1,16 @@
 import React from "react";
 import './Login.css';
-import { useHistory } from 'react-router-dom'
-interface ILoginProps {
+import { Redirect } from "react-router-dom"
+import { getPlayerName } from "../utils/Cookie";
+import LoginForm from "../components/Login/LoginForm";
 
-}
-export default class Login extends React.PureComponent<ILoginProps,{}> {
-    constructor(props: ILoginProps) {
-        super(props)
-        this.handleClick = this.handleClick.bind(this)
-    }
-    handleClick(event: React.MouseEvent) {
-        
-    }
+export default class Login extends React.PureComponent {
     render() {
-        const fieldClass = "form-field"
-        const allClass = "centered-top"
-        const buttonClass = "form-button"
+        let pageContents = getPlayerName() ? <Redirect to="/"/> : <LoginForm />
+
         return (
             <React.Fragment>
-                <div className={allClass}>
-                    <h3>Login with a name of your choice</h3>
-                    <form id="loginform" method="POST" action="/api/player">
-                        <input className={fieldClass} id="playerName" type="text" name="playerName" />
-                        <button className={buttonClass} id="loginsend" type="submit" onClick={this.handleClick}>
-                            <span>
-                                Login
-                            </span>
-                        </button>
-                    </form>
-                </div>
+                {pageContents}
             </React.Fragment>
         )
     }
