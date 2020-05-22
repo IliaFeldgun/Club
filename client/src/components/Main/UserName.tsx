0a6a43d45160be64d4f077df3b1adcbb1c835efa
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-22 19:45:43 +0300

    Decoupled cookie from component

--- client/src/components/Main/UserName.tsx
+++ client/src/components/Main/UserName.tsx
@@ -1,22 +1,14 @@
 import React from "react";
-import Cookies from 'universal-cookie';
+import { getPlayerName } from "../../utils/Cookie";
 
 export default class UserName extends React.PureComponent {
     render() {
-        const cookies = new Cookies()
-        const signedCookie = cookies.get("player_name")
-        let playerName
-        if (signedCookie) {
-            playerName = signedCookie.split(".")[0].replace("s:","")
-        }
-        // TODO: Remove after debug
-        if (!playerName)
-        {
-            playerName = <a href="/login">Login</a>
-        }
+        let playerName = getPlayerName()
+        let nameToRender = playerName ? playerName : <a href="/login">Login</a>
+
         return (
             <span className="main-user">
-                {playerName}
+                {nameToRender}
             </span>
         )
     }
