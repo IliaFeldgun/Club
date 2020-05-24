7bf68348fe3cf456ee84c7066cd47057b2764687
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-24 03:41:47 +0300

    Wiz game creation component

---
+++ client/src/components/Wiz/CreateWiz.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class CreateWiz extends React.PureComponent{
+    render() {
+        return (
+            <React.Fragment>
+                
+            </React.Fragment>
+        )
+    }
+}
