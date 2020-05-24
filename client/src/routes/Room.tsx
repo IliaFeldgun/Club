81f81c5c7f4258a3d9d8fb86334852fb2ca3398d
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-24 03:40:58 +0300

    Added display for room players

--- client/src/routes/Room.tsx
+++ client/src/routes/Room.tsx
@@ -1,35 +1,51 @@
 import React from 'react'
-import PostButton from '../components/PostButton'
-interface IRoomState{
-    roomId: string
+import { RouteComponentProps, match } from 'react-router-dom'
+import LobbyApi from '../api/LobbyApi'
+import PlayerList from '../components/PlayerList'
+import "./Room.css"
+import { getPlayerName } from '../utils/Cookie'
+import JoinButton from '../components/Room/JoinButton'
+interface IRouteParams {
+    id: string
 }
-export default class Room extends React.PureComponent<{},IRoomState> {
-    constructor() {
-        super({})
+interface IRoomProps extends RouteComponentProps<IRouteParams>{
+    match: match<IRouteParams>;
+}
+interface IRoomState {
+    players: string[]
+}
+export default class Room extends React.PureComponent<IRoomProps,IRoomState>{
+    constructor(props: IRoomProps) {
+        super(props)
 
-        
-        this.roomCreated = this.roomCreated.bind(this)
-        this.gameCreated = this.gameCreated.bind(this)
+        this.state = {
+            players: []
+        }
     }
-    roomCreated(res: Response) {
-            res.json().then((res) =>
-            {
-                this.setState({
-                    roomId: res.roomId
+    componentDidMount() {
+        LobbyApi.getRoomPlayers(this.props.match.params.id).then((res: Response) => {
+            if (res.status === 200) {
+                res.json().then((json) => {
+                    this.setState({players: [...json.playerNames]})
                 })
-            })
-    }
-    gameCreated(res: Response) {
-
+            }
+        })
     }
     render() {
+        let joinButton = <JoinButton roomId={this.props.match.params.id} />
+        if (this.state.players.some((player) => player === getPlayerName()))
+        {
+            joinButton = <React.Fragment/>
+        }
         return (
             <React.Fragment>
-                <div className="centered-top">
-                    <h3>Create a room</h3>
-                    <PostButton text="Create a room for me" route="api/room" handleResponse={this.roomCreated}/>
-                    <PostButton text="Create a game for me" route="api/game/wiz" handleResponse={this.gameCreated} body={this.state}/>
-                </div>
+            <div className="centered-top">
+                <span className="bold">This is room: </span>
+                <span>{this.props.match.params.id}</span>
+                <p className="block bold">Players in this room:</p>
+                <PlayerList players={this.state.players} />
+                {joinButton}
+            </div>
             </React.Fragment>
         )
     }
