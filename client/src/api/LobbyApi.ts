9b2dd148574bdd09fcf4be4443875e325522dfce
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-24 03:40:04 +0300

    Added room join and get room players

--- client/src/api/LobbyApi.ts
+++ client/src/api/LobbyApi.ts
@@ -3,20 +3,45 @@ export default class LobbyApi {
         const data = new URLSearchParams();
         data.append("playerName", playerName)
 
-        
         const options: RequestInit = { 
             method: "POST",
             cache: "no-cache",
             body: data
-
         }
         return fetch('/api/player', options)
     }
+
     static async newRoom() {
         const options: RequestInit = {
             method: "POST",
             cache: "no-cache",
+            headers: {
+                'Content-Type': 'application/json'
+            },
         }
         return fetch('/api/room', options)
+    }
+
+    static async getRoomPlayers(roomId: string) {
+        const options: RequestInit = {
+            method: "GET",
+            cache: "no-cache",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }
+        return fetch(`/api/room/${roomId}/players`, options)
+    }
+
+    static async joinRoom(roomId: string) {
+        const options: RequestInit = {
+            method: "POST",
+            cache: "no-cache",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }
+
+        return fetch(`/api/room/${roomId}/join`, options)
     }
 }
