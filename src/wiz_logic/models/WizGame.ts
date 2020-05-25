60adf0b0bd1154edee1792792c36250105982d76
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-25 21:34:17 +0300

    Added missing room ID to interface

--- src/wiz_logic/models/WizGame.ts
+++ src/wiz_logic/models/WizGame.ts
@@ -1,8 +1,10 @@
 import IWizScore from "./WizScore";
 import IWizRound from "./WizRound";
+import IRoom from "../../engine/lobby_logic/models/Room";
 
 export default interface IWizGame {
     id: string
+    roomId: IRoom["id"]
     currentRound: number
     currentRoundId: IWizRound["id"]
     playerScores: { [playerId: string]: IWizScore }
