78f6edd5af92dc2205973979fc5a419ec28949ca
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-25 16:33:41 +0300

    Fixed error handling to be boolean

--- src/api/room.ts
+++ src/api/room.ts
@@ -2,7 +2,7 @@ import express from "express"
 import IRoom from "../engine/lobby_logic/models/Room";
 import LobbyBuilder from "../engine/lobby_logic/LobbyBuilder";
 import LobbyMaster from "../engine/lobby_logic/LobbyMaster";
-import { REPL_MODE_SLOPPY } from "repl";
+import LobbyStore from "../engine/lobby_logic/LobbyStore";
 
 const router = express.Router()
 
@@ -10,11 +10,11 @@ router.post('/', async (req, res) => {
     const playerId = req.playerId
 
     if (playerId) {
-        try {
-            const roomId = await LobbyBuilder.createRoom(playerId)
-            res.send({ roomId})
+        const roomId = await LobbyBuilder.createRoom(playerId)
+        if(roomId) {
+            res.send({roomId: roomId})
         }
-        catch (error) {
+        else {
             res.status(500)
             res.send("FAIL")
         }
@@ -30,12 +30,12 @@ router.get('/:roomId', async (req, res) => {
     const playerId = req.playerId
 
     if (playerId) {
-        try {
-            const room: IRoom = await LobbyMaster.getRoom(roomId)
+        const room: IRoom = await LobbyStore.getRoom(roomId)
+        if (room) {
             if (room.players.indexOf(playerId) !== -1)
                 res.send(room);
         }
-        catch (error) {
+        else {
             res.status(500)
             res.send("FAIL")
         }
@@ -46,11 +46,11 @@ router.get('/:roomId/players', async (req,res) => {
     const playerId = req.playerId
 
     if (playerId) {
-        try {
-            const players = await LobbyMaster.getRoomPlayerNames(roomId)
+        const players = await LobbyMaster.getRoomPlayerNames(roomId)
+        if (players) {
             res.send({playerNames: players})
         }
-        catch (error) {
+        else {
             res.status(500)
             res.send("FAIL")
         }
@@ -58,19 +58,15 @@ router.get('/:roomId/players', async (req,res) => {
 })
 router.post('/:roomId/join', async ( req, res ) => {
     const playerId = req.playerId
+    const roomId = req.params.roomId
 
     if (playerId) {
-        const roomId = req.params.roomId
 
-        if (roomId) {
-            try {
-                const storeResponse = await LobbyMaster.addPlayerToRoom(playerId, roomId)
-
-                res.send({roomId})
-            }
-            catch (error) {
-                res.status(500).send("FAIL")
-            }
+        if (roomId && await LobbyMaster.addPlayerToRoom(playerId, roomId)) {
+            res.send({roomId})
+        }
+        else {
+            res.status(500).send("FAIL")
         }
     }
     else {
@@ -85,15 +81,11 @@ router.delete('/:roomId/player', async ( req, res ) => {
     if (playerId) {
         const roomId = req.params.roomId
 
-        if (roomId) {
-            try {
-                const storeResponse = await LobbyMaster.removePlayerFromRoom(playerId, roomId)
-
-                res.send("OK")
-            }
-            catch (error) {
-                res.status(500).send("FAIL")
-            }
+        if (roomId && await LobbyMaster.removePlayerFromRoom(playerId, roomId)) {
+            res.send("OK")
+        }
+        else {
+            res.status(500).send("FAIL")
         }
     }
     else {
@@ -107,9 +99,8 @@ router.post('/:roomId/game/:gameName', async ( req, res ) => {
     const roomId = req.params.roomId
     const gameName = req.params.gameName
 
-    try {
-        const room : IRoom = await LobbyMaster.getRoom(roomId)
-
+    const room : IRoom = await LobbyMaster.getRoom(roomId)
+    if (room) {
         if (room.leader === playerId) {
             await LobbyMaster.setRoomGameType(roomId, gameName)
             res.send({})
@@ -118,7 +109,7 @@ router.post('/:roomId/game/:gameName', async ( req, res ) => {
             res.send("You are not room leader")
         }
     }
-    catch (error) {
+    else {
         res.status(500).send("FAIL")
     }
 });
