3118ffda9fbc3569f7d0ca7d67181eb45c1b3c73
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-25 19:48:11 +0300

    Wiz now requests for game

--- client/src/routes/Wiz.tsx
+++ client/src/routes/Wiz.tsx
@@ -2,17 +2,23 @@ import React from "react";
 import './Wiz.css';
 import WizGame from "../components/Wiz/WizGame";
 import { match, RouteComponentProps } from "react-router";
+import { WizApi } from "../api/WizApi";
 
 interface IRouteParams {
-    roomId: string
+    id: string
 }
 interface IWizProps extends RouteComponentProps<IRouteParams>{
     match: match<IRouteParams>;
 }
 interface IWizState {
-
+    game: any
 }
 export default class Wiz extends React.PureComponent<IWizProps,IWizState> {
+    componentDidMount() {
+        const game = WizApi.getGame(this.props.match.params.id)
+        console.log(game)
+        this.setState({game})
+    }
     render() {
         return (
             <React.Fragment>
