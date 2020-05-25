f8f4019b61947d2d02e6ab68e4659f09742268ab
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-25 21:31:34 +0300

    state init in ctor

--- client/src/routes/Lobby.tsx
+++ client/src/routes/Lobby.tsx
@@ -5,6 +5,11 @@ interface ILobbyState {
     rooms: string[]
 }
 export default class Lobby extends React.PureComponent<{},ILobbyState>{
+    constructor({}) {
+        super({})
+
+        this.state = {rooms: []}
+    }
     render() {
         return (
             <RoomList rooms={this.state.rooms}/>
