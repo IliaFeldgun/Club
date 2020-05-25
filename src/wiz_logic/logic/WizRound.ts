1bf1daaaa2dded1d8c87d926877b3effb46d3394
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-25 19:29:57 +0300

    Added player results to constructor

--- src/wiz_logic/logic/WizRound.ts
+++ src/wiz_logic/logic/WizRound.ts
@@ -30,6 +30,7 @@ export default class WizRound implements IWizRound {
         this.playerOrder = []
         this.playerHands = {}
         this.playerBets = {}
+        this.playerResults = {}
 
     }
 }
