8818b34773616c33ae38663598c2759fbf9e3620
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-25 16:34:21 +0300

    Fixed error handling to be boolean

--- src/api/player.ts
+++ src/api/player.ts
@@ -1,21 +1,19 @@
 import express from "express"
 import LobbyBuilder from "../engine/lobby_logic/LobbyBuilder"
-import LobbyMaster from "../engine/lobby_logic/LobbyMaster"
+import LobbyStore from "../engine/lobby_logic/LobbyStore"
 
 const router = express.Router()
 
 router.post('/', async (req, res) => {
     if (!req.signedCookies.player_id) {
         const playerName = req.body.playerName
-
-        try {
-            const playerId = await LobbyBuilder.createPlayer(playerName)
-
+        const playerId = await LobbyBuilder.createPlayer(playerName)
+        if (playerId) {
             res.cookie("player_name", playerName, { signed: true })
             res.cookie("player_id", playerId, { signed: true })
             res.send("Player created, cookie sent")
-        }
-        catch(error) {
+        }   
+        else {
             res.status(500)
             res.send("FAIL")
         }
@@ -29,13 +27,12 @@ router.delete('/', async (req, res) => {
     const playerId = req.playerId
 
     if (playerId) {
-        try {
-            const storeResponse = await LobbyMaster.deletePlayer(playerId)
+        if (await LobbyStore.deletePlayer(playerId)) {
             res.clearCookie("player_name", { signed: true })
             res.clearCookie("player_id", { signed: true })
             res.send("Player deleted, cookie deleted")
         }
-        catch (error) {
+        else {
             res.status(500)
             res.send("FAIL")
         }
