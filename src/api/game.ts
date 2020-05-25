1db07c229411eff623f23e7a734dd6039820d969
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-25 19:58:39 +0300

    Request body fixed to params

--- src/api/game.ts
+++ src/api/game.ts
@@ -43,7 +43,7 @@ router.post('/wiz/:roomId', async ( req, res ) => {
 router.get('/wiz/:gameId', async (req, res) => {
     const playerId = req.playerId
     if (playerId) {
-        const gameId = req.body.gameId
+        const gameId = req.params.gameId
         const game = await WizStore.getWizGame(gameId)
         res.send({game})
     }
@@ -54,7 +54,7 @@ router.get('/wiz/:gameId', async (req, res) => {
 router.get('/wiz/:gameId/round', async (req, res) => {
     const playerId = req.playerId
     if (playerId) {
-        const gameId = req.body.gameId
+        const gameId = req.params.gameId
         const round = await WizMaster.getWizRoundByGame(gameId)
         res.send({round})
     }
@@ -62,24 +62,7 @@ router.get('/wiz/:gameId/round', async (req, res) => {
         res.status(403).send("Player needs be set")
     }
 })
-/*router.get('/wiz', (req, res) => {
-    const playerId = req.signedCookies.player_id
-    const gameId = req.body.gameId
-    store.getAsync()(gameId).then((gameJson : string) => {
-        const game : IWizGame = JSON.parse(gameJson)
-        if (game.players.findIndex(player => player == playerId) != -1) {
-            res.send()
-        }
-        else {
-            res.send("You are not in the specified room")
-        }
-    }).catch((err: any) => {
-        res.status(500)
-        res.send("FAIL")
-    })
 
-})
-*/
 router.post('/wiz/bet/:bet', (req, res) => {
     res.send("Bet submitted")
 })
