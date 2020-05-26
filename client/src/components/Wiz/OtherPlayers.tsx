c3451095d8ec8aa642bc9d09c360c5b52591a4aa
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-26 16:54:17 +0300

    Rendering other players

--- client/src/components/Wiz/OtherPlayers.tsx
+++ client/src/components/Wiz/OtherPlayers.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import EmptyCardFan from './EmptyCardFan'
+import WizOtherPlayer from './OtherPlayer'
 
 interface IWizOtherPlayersProps {
     players: Array<{name: string, cards: number}>
@@ -8,7 +9,12 @@ export default class WizOtherPlayers extends React.PureComponent<IWizOtherPlayer
     render() {
         const cards = this.props.players.map((player, index) => {
             return (
-                <EmptyCardFan className={`other-fan-${index + 1}`} key={player.name} cards={player.cards}/>
+                <React.Fragment>
+                    <WizOtherPlayer key={player.name} 
+                                    name={player.name} 
+                                    cards={player.cards} 
+                                    className={`other-fan-${index + 1}`}/>
+                </React.Fragment>
             )
         })
         return (
