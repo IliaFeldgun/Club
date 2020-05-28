f67bcf247cdc83db13deda1b511ee47d06460b58
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-28 17:48:39 +0300

    Unnecessary import

--- src/engine/player_assert.ts
+++ src/engine/player_assert.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from "express"
+import { Request, Response, NextFunction } from "express"
 
 export default function assertPlayer(req : Request, res: Response, next: NextFunction) {
     if (req.signedCookies.player_id) {
