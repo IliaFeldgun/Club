fb722ee63663ca903a115a3db02aa35b250a0a13
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-29 20:06:33 +0300

    Hidden until used properly

--- client/src/components/Main/Footer.tsx
+++ client/src/components/Main/Footer.tsx
@@ -3,10 +3,11 @@ import React from "react";
 export default class Footer extends React.PureComponent{
     render() {
         return (
-            <footer className="bottom fixed horizontal-centered wide">
-                <p>No rights reserved. We're collecting all your data!</p>
-                {this.props.children}
-            </footer>
+            // <footer className="bottom fixed horizontal-centered wide">
+            //     <p>No rights reserved. We're collecting all your data!</p>
+            //     {this.props.children}
+            // </footer>
+            <React.Fragment/>
         )
     }
 }
