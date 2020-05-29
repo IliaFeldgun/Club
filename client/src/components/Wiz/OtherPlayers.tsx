ff35735d1008c8ae1818aea6f19d521c78bc15d7
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-29 14:08:20 +0300

    Removed unnecessary fragment

--- client/src/components/Wiz/OtherPlayers.tsx
+++ client/src/components/Wiz/OtherPlayers.tsx
@@ -9,12 +9,10 @@ export default class WizOtherPlayers extends React.PureComponent<IWizOtherPlayer
     render() {
         const cards = this.props.players.map((player, index) => {
             return (
-                <React.Fragment>
                     <WizOtherPlayer key={player.name} 
                                     name={player.name} 
                                     cards={player.cards} 
                                     className={`other-fan-${index + 1}`}/>
-                </React.Fragment>
             )
         })
         return (
