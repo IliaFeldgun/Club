fa60570fb1e02176595ede1b42375bc750206f56
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-29 12:46:28 +0300

    Added getter for player hands in API

--- client/src/api/WizApi.ts
+++ client/src/api/WizApi.ts
@@ -47,4 +47,14 @@ export class WizApi {
         }
         return fetch(`/api/game/wiz/${gameId}/handsizes`, options)
     }
+    static getPlayerHand(gameId: string) {
+        const options: RequestInit = {
+            method: "GET",
+            cache: "no-cache",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }
+        return fetch(`/api/game/wiz/${gameId}/hand`)
+    }
 }
