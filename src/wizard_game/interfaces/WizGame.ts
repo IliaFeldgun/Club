25889c52ee61df88ed593952fb7f47639ac124f9
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-30 14:22:37 +0300

    Player order and game finish added

--- src/wizard_game/interfaces/WizGame.ts
+++ src/wizard_game/interfaces/WizGame.ts
@@ -1,11 +1,14 @@
 import IWizScore from "./WizScore";
 import IWizRound from "./WizRound";
 import IRoom from "../../engine/lobby/interfaces/Room";
+import IPlayer from "../../engine/lobby/interfaces/Player";
 
 export default interface IWizGame {
     id: string
     roomId: IRoom["id"]
     currentRound: number
     currentRoundId: IWizRound["id"]
+    playerOrder: IPlayer["id"][]
     playerScores: { [playerId: string]: IWizScore }
+    isDone: boolean
 }
--- src/wizard_game/models/WizGame.ts
+++ src/wizard_game/models/WizGame.ts
@@ -2,19 +2,24 @@ import IWizGame from "../interfaces/WizGame"
 import IWizScore from "../interfaces/WizScore"
 import IRoom from "../../engine/lobby/interfaces/Room"
 import IWizRound from "../interfaces/WizRound"
+import IPlayer from "../../engine/lobby/interfaces/Player"
 
 export default class WizGame implements IWizGame {
     id: string
     roomId: IRoom["id"]
     currentRound: number
     currentRoundId: IWizRound["id"]
+    playerOrder: IPlayer["id"][]
     playerScores: { [playerId: string]: IWizScore }
+    isDone: boolean
 
     constructor(id: IWizGame["id"], roomId: IRoom["id"]) {
         this.id = id
         this.roomId = roomId
         this.currentRound = 0
         this.currentRoundId = ""
+        this.playerOrder = []
         this.playerScores = {}
+        this.isDone = false
     }
 }
