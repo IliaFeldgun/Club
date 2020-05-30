21ef5ad8a98fc5ea3ab806e9d2a9a9708661536a
Ilia Feldgun <iliafeldgun@outlook.com>
2020-05-30 19:34:01 +0300

    Added bet display in list

--- client/src/components/Wiz/PlayerList.tsx
+++ client/src/components/Wiz/PlayerList.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 interface IWizPlayerListProps {
     players: Array<{id: string, name: string, score: number}>
+    playerBets: { [playerId: string]: number }
 }
 export default class WizPlayerList extends React.PureComponent<IWizPlayerListProps,{}>{
     render() {
-        const players = this.props.players.map((player) => 
-            <li key={player.id}>{player.name}: {player.score}</li>
-        )
+        const players = this.props.players.map((player) => {
+            const playerBet = this.props.playerBets[player.id]
+            let betText = "Bet: "
+            if (!playerBet) {
+                betText += "None"
+            }
+            else {
+                betText += playerBet
+            }
+
+            return <li key={player.id}>{player.name}: {player.score} {betText}</li>
+        })
         return (
             <ol>
                 {players}
