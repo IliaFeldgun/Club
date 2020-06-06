5ddd67339dc12c04e0cd5943d4cd1715c57095c3
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-06 19:19:35 +0300

    ClassName sent

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -6,7 +6,7 @@ import LoginForm from "../components/Login/LoginForm";
 
 export default class Login extends React.PureComponent {
     render() {
-        let pageContents = getPlayerName() ? <Redirect to="/"/> : <LoginForm />
+        let pageContents = getPlayerName() ? <Redirect to="/"/> : <LoginForm className="centered-top"/>
 
         return (
             <React.Fragment>
