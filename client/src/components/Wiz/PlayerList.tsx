3d76d885af54f939150c802ccf78fd37fe3bc796
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-07 14:48:44 +0300

    Displays next player

--- client/src/components/Wiz/PlayerList.tsx
+++ client/src/components/Wiz/PlayerList.tsx
@@ -2,21 +2,29 @@ import React from 'react'
 interface IWizPlayerListProps {
     players: Array<{id: string, name: string, score: number}>
     playerBets: { [playerId: string]: number }
+    nextPlayer: string
 }
 export default class WizPlayerList extends React.PureComponent<IWizPlayerListProps,{}>{
     render() {
-        const players = this.props.players.map((player) => {
-            const playerBet = this.props.playerBets[player.id]
-            let betText = "Bet: "
-            if (playerBet === undefined) {
-                betText += "None"
-            }
-            else {
-                betText += playerBet
-            }
-
-            return <li key={player.id}>{player.name}: {player.score} {betText}</li>
-        })
+        let players: JSX.Element[] = [<React.Fragment/>]
+        
+        if (this.props.players) {
+            players = this.props.players.map((player) => {
+                const playerBet = this.props.playerBets[player.id]
+                let betText = "Bet: "
+                let classes = ""
+                if (playerBet === undefined) {
+                    betText += "None"
+                }
+                else {
+                    betText += playerBet
+                }
+                if (player.id === this.props.nextPlayer) {
+                    classes = "highlight"
+                }
+                return <li className={classes} key={player.id}>{player.name}: {player.score} {betText}</li>
+            })
+        }
         return (
             <ol>
                 {players}
