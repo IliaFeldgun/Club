32a0006fc9f88910dc6fda257ac8d9daad7fdd21
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-11 22:20:00 +0300

    Refactored and added more CRUD

--- src/engine/database.ts
+++ src/engine/database.ts
@@ -3,33 +3,25 @@ import MongoDBClient from './mongodb'
 const DB_NAME = process.env.MONGODB_DB
 
 export default class Database {
-    static async insert(collectionName: string, object: any): Promise<boolean> {
+    private static async getCollectionByName(collectionName: string) {
         const mongoClient = (await MongoDBClient.getClient())
-        const collection = mongoClient.db(DB_NAME).collection(collectionName)
-        const result = await collection.insertOne(object)
-        if (result.insertedCount === 1) {
-            return true
+        if (mongoClient) {
+            const db = mongoClient.db(DB_NAME)
+            if (db) {
+                return db.collection(collectionName)
+            }
+            else {
+                // TODO: handle
+                return undefined
+            }
         }
         else {
-            return false
+            // TODO: handle
+            return undefined
         }
-    }
-    static async replace(collectionName: string, id: string, object: any):
-        Promise<boolean> {
-        const mongoClient = (await MongoDBClient.getClient())
-        const collection = mongoClient.db(DB_NAME).collection(collectionName)
-        const result = await collection.replaceOne({id}, object)
-        if (result.modifiedCount === 1) {
-            return true
-        }
-        else {
-            return false
-        }
-
     }
     static async get(collectionName: string, field: string, value: string) {
-        const mongoClient = (await MongoDBClient.getClient())
-        const collection = mongoClient.db(DB_NAME).collection(collectionName)
+        const collection = await Database.getCollectionByName(collectionName)
 
         const filter: any = {}
         filter[field] = value
@@ -40,4 +32,66 @@ export default class Database {
             // TODO: handle
         }
     }
+    static async insert(collectionName: string, object: any): Promise<boolean> {
+        const collection = await Database.getCollectionByName(collectionName)
+        const result = await collection.insertOne(object)
+        if (result.insertedCount === 1) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    static async replace(collectionName: string, idToReplace: string, object: any):
+        Promise<boolean> {
+        const collection = await Database.getCollectionByName(collectionName)
+        const result = await collection.replaceOne({idToReplace}, object)
+        if (result.modifiedCount === 1) {
+            return true
+        }
+        else {
+            return false
+        }
+
+    }
+    static async update(collectionName: string, idToUpdate: string, field: string, value: string):
+        Promise<boolean> {
+            const collection = await Database.getCollectionByName(collectionName)
+            const update: any = {}
+            update[field] = value
+            const result = await collection.updateOne({idToUpdate},update)
+            if (result.modifiedCount === 1) {
+                return true
+            }
+            else {
+                return false
+            }
+    }
+    static async pushToArray(collectionName: string, idToUpdate: string, arrayName: string, value: string):
+        Promise<boolean> {
+            const collection = await Database.getCollectionByName(collectionName)
+            const toPush: any = {}
+            toPush[arrayName] = value
+            const result  = await collection.updateOne({idToUpdate}, {$push: toPush})
+            if (result.modifiedCount === 1) {
+                return true
+            }
+            else {
+                return false
+            }
+    }
+    static async pullFromArray(collectionName: string, idToUpdate: string, arrayName: string, value: string):
+        Promise<boolean> {
+            const collection = await Database.getCollectionByName(collectionName)
+            const toPull: any = {}
+            toPull[arrayName] = value
+            const result = await collection.updateOne({idToUpdate}, {$pull: {toPull}})
+            if (result.modifiedCount === 1) {
+                return true
+            }
+            else {
+                return false
+            }
+
+        }
 }
