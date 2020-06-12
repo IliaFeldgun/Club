f763c36d4f32e993526f18d5546e66c4bb5f9db9
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-12 18:43:39 +0300

    Game image proper now

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.css"
 import Description from "../components/Home/Description";
 import Catalog from "../components/Home/Catalog";
+import wizGameImage from '../img/WizClub2Small.png'
 interface IHomeProps {
 
 }
@@ -20,7 +21,7 @@ export default class Home extends React.PureComponent<IHomeProps,IHomeState> {
             <React.Fragment>
                 <Description/>
                 <Catalog games={[{name: "Wizard", url: "/room",
-                                  img:"https://norskpokerforbund.com/wp-content/uploads/2018/08/Card-Game.jpg"}]} />
+                                  img: wizGameImage}]} />
             </React.Fragment>
         )
     }
