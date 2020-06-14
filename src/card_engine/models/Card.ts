2dbb07ebe6011ad436dd8260efb08b8bc691847b
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-14 15:55:29 +0300

    Members to be removed

--- src/card_engine/models/Card.ts
+++ src/card_engine/models/Card.ts
@@ -3,34 +3,34 @@ import ICard, { Rank, Suit, Color } from "../interfaces/Card"
 export default class Card implements ICard {
     suit: Suit
     rank: Rank
-    face: boolean
+    // face: boolean
     // DeckId: string
     // StackId: string
 
-    constructor(suit: Suit, rank: Rank, face: boolean, deckId?: string){
+    constructor(suit: Suit, rank: Rank){
         this.suit = suit
         this.rank = rank
-        this.face = face
+        // this.face = face
     }
 
-    flip() {
-        this.face = !this.face
-    }
+    // flip() {
+    //     this.face = !this.face
+    // }
 
-    getColor() {
-        let color : Color
+    // getColor() {
+    //     let color : Color
 
-        if (this.suit === Suit.HEART || this.suit === Suit.DIAMOND)
-        {
-            color = Color.RED
-        }
-        else
-        {
-            color = Color.BLACK
-        }
+    //     if (this.suit === Suit.HEART || this.suit === Suit.DIAMOND)
+    //     {
+    //         color = Color.RED
+    //     }
+    //     else
+    //     {
+    //         color = Color.BLACK
+    //     }
 
-        return color
-    }
+    //     return color
+    // }
 
     static equals(card1: ICard, card2: ICard): boolean {
         return (card1.rank === card2.rank && card1.suit === card2.suit)
