520d7432c3f8e6a128228255cd0cc14fed13ade6
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-22 18:11:51 +0300

    Removed unnecessary dependencies

--- src/wizard_game/models/WizScore.ts
+++ src/wizard_game/models/WizScore.ts
@@ -1,6 +1,3 @@
-import WizGameRules from "../WizGameRules"
-import IWizBet from "../interfaces/WizBet"
-import IWizPlayerRoundResult from "../interfaces/WizPlayerRoundResult"
 import IWizScore from "../interfaces/WizScore"
 
 export default class WizScore implements IWizScore {
