83fe74f21d2ba1beba183db5db5c0a6f4cbc43a9
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-22 18:25:05 +0300

    Winner calculation

--- src/wizard_game/WizInfo.ts
+++ src/wizard_game/WizInfo.ts
@@ -6,6 +6,7 @@ import Card from "../card_engine/models/Card"
 import Stack from "../card_engine/models/Stack"
 import { PossibleMoves } from "./enums/PossibleMoves"
 import IWizGame from "./interfaces/WizGame"
+import WizScore from "./models/WizScore"
 
 export default class WizInfo {
     static getPlayerByCard(round: IWizRound, card: ICard): IPlayer["id"] {
@@ -112,5 +113,14 @@ export default class WizInfo {
         const totalRounds =
             WizGameRules.getTotalRounds(game.playerOrder.length)
         return game.currentRound.roundNumber > totalRounds
+    }
+    static getGameWinner(game: IWizGame): IPlayer["id"] {
+        const maxScore = WizScore.max(Object.values(game.playerScores))
+        const winner = Object.entries(game.playerScores).find(([player, score]) => {
+            return score.total === maxScore
+        })
+
+        return winner[0]
+
     }
 }
