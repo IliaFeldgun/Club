297b6c18a554fdd24c2ff0bcd23caa95b42fc8df
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-22 18:18:24 +0300

    Maximum score calculator

--- src/wizard_game/models/WizScore.ts
+++ src/wizard_game/models/WizScore.ts
@@ -6,4 +6,13 @@ export default class WizScore implements IWizScore {
     constructor() {
         this.total = 0
     }
+    static max(scores: WizScore[]) {
+        const scoreNumbers = scores.map((score) => {
+            return score.total
+        })
+        const maxScore = scoreNumbers.reduce((a,b) => {
+            return Math.max(a,b)
+        })
+        return maxScore
+    }
 }
