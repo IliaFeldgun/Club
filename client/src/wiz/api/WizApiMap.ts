6517e3fcdc10463dd78e856d70a88cf2476da163
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-24 20:10:52 +0300

    Mapped the API

--- client/src/wiz/api/WizApiMap.ts
+++ client/src/wiz/api/WizApiMap.ts
@@ -1,19 +1,92 @@
+import { AxiosRequestConfig } from 'axios'
+import ICard from '../../interfaces/Card'
+
+const POST_CONFIG: AxiosRequestConfig = {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache'
+    }
+}
+const GET_CONFIG: AxiosRequestConfig = {
+    method: "GET",
+    headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache'
+    }
+}
 const WIZ_API_MAP = {
-    WIZ: {
-        SEND_CARD: {
-            URL: "/api/game/wizard",
-            METHOD: "POST"
+    NEW_GAME: {
+        url: (roomId: string) => {
+            return `/api/game/wizard/${roomId}`
+        },
+        config: () => {
+            return {...POST_CONFIG}
         }
     },
-    LOBBY: {
-        CREATE_PLAYER: {
-            URL: "/api/player",
-            METHOD: "POST"
+    GET_GAME_INSTRUCTIONS: {
+        url: (gameId: string) => {
+            return `/api/game/wizard/${gameId}`
         },
-        DELETE_PLAYER: {
-            URL: "/api/player",
-            METHO: "DELETE"
+        config: () => {
+            return {...GET_CONFIG}
+        }
+    },
+    GET_GAME_PLAYERS: {
+        url: (gameId: string) => {
+            return `/api/game/wizard/${gameId}/players`
         },
+        config: () => {
+            return {...GET_CONFIG}
+        }
+    },
+    GET_NEXT_PLAYER: {
+        url: (gameId: string) => {
+            return `/api/game/wizard/${gameId}/nextplayer`
+        },
+        config: () => {
+            return {...GET_CONFIG}
+        }
+    },
+    GET_PLAYER_HAND: {
+        url: (gameId: string) => {
+            return `/api/game/wizard/${gameId}/hand`
+        },
+        config: () => {
+            return {...GET_CONFIG}
+        }
+    },
+    GET_TABLE_STACK: {
+        url: (gameId: string) => {
+            return `/api/game/wizard/${gameId}/stack`
+        },
+        config: () => {
+            return {...GET_CONFIG}
+        }
+    },
+    GET_STRONG_SUIT: {
+        url: (gameId: string) => {
+            return `/api/game/wizard/${gameId}/kozer`
+        },
+        config: () => {
+            return {...GET_CONFIG}
+        }
+    },
+    SEND_CARD: {
+        url: (gameId: string) => {
+            return `/api/game/wizard/${gameId}/play`
+        },
+        config: (card: ICard) => {
+            return {...POST_CONFIG, data: JSON.stringify(card)}
+        }
+    },
+    SEND_BET: {
+        url: (gameId: string, bet: number) => {
+            return `/api/game/wizard/${gameId}/bet/${bet}`
+        },
+        config: () => {
+            return {...POST_CONFIG}
+        }
     }
 }
 
