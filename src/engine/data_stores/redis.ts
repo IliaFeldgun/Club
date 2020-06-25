ff5444d64f1cc1a2c9dd315f2460fe4c4c350c7d
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-25 20:07:32 +0300

    Removed old comments

--- src/engine/data_stores/redis.ts
+++ src/engine/data_stores/redis.ts
@@ -19,19 +19,11 @@ function newClient() {
 }
 function newPublisherClient() {
     const client = newClient()
-    // client.config('get', 'notify-keyspace-events', function(err, success) {
-    //     if (err) {
-    //         // TODO: Handle, Log
-    //     }
-    //     if (!success) {
-    // TODO: For some reason Es doesn't work, need to investigate
     client.config('set', 'notify-keyspace-events', 'EA', (err) => {
         if (err) {
             // TODO: Handle, Log
         }
     });
-    //     }
-    // });
     return client
 }
 // export let redLock = new RedLock(
