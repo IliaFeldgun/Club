c11a85fbfc788db7555ac612b9b2f73d8f818ace
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-26 19:47:07 +0300

    Changed object literals to functions for copying

--- client/src/wiz/api/WizApiMap.ts
+++ client/src/wiz/api/WizApiMap.ts
@@ -1,18 +1,23 @@
 import { AxiosRequestConfig } from 'axios'
 import ICard from '../../interfaces/Card'
 
-const POST_CONFIG: AxiosRequestConfig = {
-    method: "POST",
-    headers: {
-        'Content-Type': 'application/json',
-        'Cache-Control': 'no-cache'
+// functions are to avoid cloning
+function POST_CONFIG(): AxiosRequestConfig {
+    return {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
+        }
     }
 }
-const GET_CONFIG: AxiosRequestConfig = {
-    method: "GET",
-    headers: {
-        'Content-Type': 'application/json',
-        'Cache-Control': 'no-cache'
+function GET_CONFIG(): AxiosRequestConfig {
+    return {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache'
+        }
     }
 }
 const WIZ_API_MAP = {
@@ -20,73 +25,57 @@ const WIZ_API_MAP = {
         url: (roomId: string) => {
             return `/api/game/wizard/${roomId}`
         },
-        config: () => {
-            return {...POST_CONFIG}
-        }
+        config: GET_CONFIG
     },
     GET_GAME_INSTRUCTIONS: {
         url: (gameId: string) => {
             return `/api/game/wizard/${gameId}`
         },
-        config: () => {
-            return {...GET_CONFIG}
-        }
+        config: GET_CONFIG
     },
     GET_GAME_PLAYERS: {
         url: (gameId: string) => {
             return `/api/game/wizard/${gameId}/players`
         },
-        config: () => {
-            return {...GET_CONFIG}
-        }
+        config: GET_CONFIG
     },
     GET_NEXT_PLAYER: {
         url: (gameId: string) => {
             return `/api/game/wizard/${gameId}/nextplayer`
         },
-        config: () => {
-            return {...GET_CONFIG}
-        }
+        config: GET_CONFIG
     },
     GET_PLAYER_HAND: {
         url: (gameId: string) => {
             return `/api/game/wizard/${gameId}/hand`
         },
-        config: () => {
-            return {...GET_CONFIG}
-        }
+        config: GET_CONFIG
     },
     GET_TABLE_STACK: {
         url: (gameId: string) => {
             return `/api/game/wizard/${gameId}/stack`
         },
-        config: () => {
-            return {...GET_CONFIG}
-        }
+        config: GET_CONFIG
     },
     GET_STRONG_SUIT: {
         url: (gameId: string) => {
             return `/api/game/wizard/${gameId}/kozer`
         },
-        config: () => {
-            return {...GET_CONFIG}
-        }
+        config: GET_CONFIG
     },
     SEND_CARD: {
         url: (gameId: string) => {
             return `/api/game/wizard/${gameId}/play`
         },
         config: (card: ICard) => {
-            return {...POST_CONFIG, data: JSON.stringify(card)}
+            return {...POST_CONFIG(), data: JSON.stringify(card)}
         }
     },
     SEND_BET: {
         url: (gameId: string, bet: number) => {
             return `/api/game/wizard/${gameId}/bet/${bet}`
         },
-        config: () => {
-            return {...POST_CONFIG}
-        }
+        config: POST_CONFIG
     }
 }
 
