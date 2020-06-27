834351ddafa19037f2324d85cda8b106b195d6e6
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-27 15:51:41 +0300

    Logged redis connection process and refactored

--- src/engine/data_stores/redis.ts
+++ src/engine/data_stores/redis.ts
@@ -1,29 +1,32 @@
 import redis from "redis"
+import logger from '../winston'
 // import RedLock from 'redlock'
 
 export const redisClient = newClient()
 export const redisPublisher = newPublisherClient()
+const port = Number.parseInt(process.env.REDIS_PORT, 10)
+const host = process.env.REDIS_HOST
+const password = process.env.REDIS_PASSWORD
 
 function newClient() {
-    const client = redis.createClient({
-        port: Number.parseInt(process.env.REDIS_PORT, 10),
-        host: process.env.REDIS_HOST,
-        password: process.env.REDIS_PASSWORD,
+    const client = redis.createClient({port, host, password})
+
+    client.on('error', (channel, message) => {
+        logger.error("Redis error; ", message)
     })
 
-    client.on('error', (error) => {
-        // TODO: Log
-    })
-
+    logger.info("Redis client successfully connected")
     return client
 }
 function newPublisherClient() {
     const client = newClient()
     client.config('set', 'notify-keyspace-events', 'EA', (err) => {
         if (err) {
-            // TODO: Handle, Log
+            logger.error("Failed to set Redis notification; ", err)
         }
     });
+
+    logger.info("Redis publisher client successfully connected")
     return client
 }
 // export let redLock = new RedLock(
