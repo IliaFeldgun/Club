18ab769fa26705336dab379240ade9e2ec774082
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-27 18:58:39 +0300

    Validates player ID

--- src/engine/request_handlers/player_assert.ts
+++ src/engine/request_handlers/player_assert.ts
@@ -1,7 +1,10 @@
 import { Request, Response, NextFunction } from "express"
+const ID_LENGTH = "00000000-0000-0000-0000-000000000000".length
+import Validator from 'validator'
 
 export default function assertPlayer(req : Request, res: Response, next: NextFunction) {
-    if (req.signedCookies.player_id) {
+    const cookiePlayerId = req.signedCookies.player_id
+    if (cookiePlayerId && Validator.isUUID(cookiePlayerId)) {
         req.playerId = req.signedCookies.player_id
     }
     else {
