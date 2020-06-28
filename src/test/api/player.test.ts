17b11f6f102d6701b9bafc4536967ecfab0d16f1
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-28 20:14:11 +0300

    Works with app and not server

--- src/test/api/player.test.ts
+++ src/test/api/player.test.ts
@@ -1,40 +1,41 @@
 import request from 'supertest'
-import server from '../../index'
+import app from '../../api/app'
 import Validator from 'validator'
 
-describe("Testing player routes", () => {
+describe("Testing player login", () => {
     const apiRoute = "/api/player"
     test('Player creation', async () => {
         const playerName = "testplayer123"
-        const res = await request(server)
+        const res = await request(app)
             .post(apiRoute)
             .send({playerName})
         expect(res.status).toBe(200)
+        const cookie = res.get("Set-Cookie")
+        expect(cookie.length).toBe(2)
         expect(Validator.isUUID(res.body.playerId)).toBe(true)
     })
     test('Player creation with invalid characters', async () => {
         const playerName = "$badName;--injectwhatever"
-        const res = await request(server)
+        const res = await request(app)
             .post(apiRoute)
             .send({playerName})
             expect(res.status).toBe(400)
     })
     test('Player creation with no name', async () => {
         const playerName = ""
-        const res = await request(server)
+        const res = await request(app)
             .post(apiRoute)
             .send({playerName})
         expect(res.status).toBe(400)
     })
     test('Player creation with cookie', async () => {
         const playerName = "testplayer123"
-        const res = await request(server)
+        const res = await request(app)
             .post(apiRoute)
             .send({playerName})
         const playerName2 = "diffplayer321"
         const cookie = res.get("Set-Cookie")
-        console.log(cookie)
-        const res2 = await request(server)
+        const res2 = await request(app)
             .post(apiRoute)
             .set("Cookie", cookie)
             .send({playerName: playerName2})
