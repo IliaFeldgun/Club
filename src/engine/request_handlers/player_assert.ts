758b7f764b74a7b5afc86333329d07d0c1fa9ce7
Ilia Feldgun <iliafeldgun@outlook.com>
2020-07-04 16:46:21 +0300

    PlayerId now comes from session

--- src/engine/request_handlers/player_assert.ts
+++ src/engine/request_handlers/player_assert.ts
@@ -1,14 +1,6 @@
 import { Request, Response, NextFunction } from "express"
-const ID_LENGTH = "00000000-0000-0000-0000-000000000000".length
-import Validator from 'validator'
 
 export default function assertPlayer(req : Request, res: Response, next: NextFunction) {
-    const cookiePlayerId = req.signedCookies.player_id
-    if (cookiePlayerId && Validator.isUUID(cookiePlayerId)) {
-        req.playerId = req.signedCookies.player_id
-    }
-    else {
-        req.playerId = null
-    }
+    req.playerId = req.session.playerId
     next()
 }
