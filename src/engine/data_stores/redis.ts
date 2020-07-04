e2047da657c99aa7a5492706b71f7b0a34a98c76
Ilia Feldgun <iliafeldgun@outlook.com>
2020-07-04 18:20:56 +0300

    Clients have names now, for logging

--- src/engine/data_stores/redis.ts
+++ src/engine/data_stores/redis.ts
@@ -2,10 +2,10 @@ import redis from "redis"
 import logger from '../winston'
 // import RedLock from 'redlock'
 
-export const redisClient = newClient()
-export const redisPublisher = newPublisherClient()
-export const redisSessionClient = newClient()
-function newClient() {
+export const redisClient = newClient("Game Redis client")
+export const redisPublisher = newPublisherClient("Announcer Redis client")
+export const redisSessionClient = newClient("Session Redis client")
+function newClient(clientName: string) {
     const port = Number.parseInt(process.env.REDIS_PORT, 10)
     const host = process.env.REDIS_HOST
     const password = process.env.REDIS_PASSWORD
@@ -13,21 +13,20 @@ function newClient() {
     const client = redis.createClient({port, host, password})
 
     client.on('error', (channel, message) => {
-        logger.error("Redis error; ", message)
+        logger.error(`${clientName} error; `, message)
     })
 
-    logger.info("Redis client successfully connected")
+    logger.info(`${clientName} successfully connected`)
     return client
 }
-function newPublisherClient() {
-    const client = newClient()
+function newPublisherClient(clientName: string) {
+    const client = newClient(clientName)
     client.config('set', 'notify-keyspace-events', 'EA', (err) => {
         if (err) {
             logger.error("Failed to set Redis notification; ", err)
         }
     });
 
-    logger.info("Redis publisher client successfully connected")
     return client
 }
 // export let redLock = new RedLock(
