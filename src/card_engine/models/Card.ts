fc9e71376645f5104742fcc773a539bf86cf3b11
Ilia Feldgun <iliafeldgun@outlook.com>
2020-07-05 14:32:16 +0300

    Refactored

--- src/card_engine/models/Card.ts
+++ src/card_engine/models/Card.ts
@@ -1,41 +1,18 @@
 import ICard, { Rank, Suit, Color } from "../interfaces/Card"
-import { isNumber } from "util"
 
 export default class Card implements ICard {
     suit: Suit
     rank: Rank
-    // face: boolean
-    // DeckId: string
-    // StackId: string
 
     constructor(suit: Suit, rank: Rank){
         this.suit = suit
         this.rank = rank
-        // this.face = face
     }
-
-    // flip() {
-    //     this.face = !this.face
-    // }
-
-    // getColor() {
-    //     let color : Color
-
-    //     if (this.suit === Suit.HEART || this.suit === Suit.DIAMOND)
-    //     {
-    //         color = Color.RED
-    //     }
-    //     else
-    //     {
-    //         color = Color.BLACK
-    //     }
-
-    //     return color
-    // }
-
     static isCard(card: ICard): boolean {
         const hasTwoProps = Object.keys(card).length === 2
-        return hasTwoProps && isNumber(card.rank) && isNumber(card.suit)
+        const rankOkay = card.rank >= Rank.TWO && card.rank <= Rank.JOKER
+        const suitOkay = card.suit >= Suit.HEART && card.suit <= Suit.SPADE
+        return hasTwoProps && rankOkay && suitOkay
     }
     static equals(card1: ICard, card2: ICard): boolean {
         return (card1.rank === card2.rank && card1.suit === card2.suit)
